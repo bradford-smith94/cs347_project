b0eaf4bfed3e01cfee8c2182e045ce82c8af2c27
David <davidott94@gmail.com>
2015-04-22 20:54:50 -0400

    there they are

---
+++ src/person.py
@@ -0,0 +1,11 @@
+#Bradford Smith and David Ott
+#cs347 asssignment 3
+#Absence check person.py
+#I pledge my honor that I have abided by the stevens honor system
+
+
+class person:
+	def __init__(self, name, email):
+		self.name = name;
+		self.email = email;
+
