cb19a4882bb17b831226e2076b141ca19d46889f
Bradford Smith <bsmith8@stevens.edu>
2015-04-23 11:01:17 -0400

    Added comments
    added comments to person and student

--- src/person.py
+++ src/person.py
@@ -4,13 +4,20 @@
 # "I pledge my honor that I have abided by the Stevens Honor System."
 #####################################################################
 
+#person
+#this class provides the structure of a person with a name and an email
 class person(object):
+    #constructor
     def __init__(self, name, email):
         self.name = name
         self.email = email
 
+    #pre:none
+    #post:returns the name variable
     def get_name():
         return self.name
 
+    #pre:none
+    #post:returns the email variable
     def get_email():
         return self.email
--- src/student.py
+++ src/student.py
@@ -3,19 +3,28 @@
 # AbsenceCheck student.py
 # "I pledge my honor that I have abided by the Stevens Honor System."
 #####################################################################
+#import person class
 from person import person
 
+#student
+#this class provides the structure of a student
+#   student is a person with attendance
 class student(person):
 
+    #constructor
     def __init__(self, name, email, here=True):
         person.__init__(self, name, email)
         self.isHere = here #assume a student is here until otherwise known
 
+    #pre:takes in a boolean here (True = student present, False = student absent)
+    #post:sets the isHere variable to here
     def set_attendance(here):
         if(here):
             isHere = True
         else:
             isHere = False
 
+    #pre:none
+    #post:returns the isHere variable
     def get_attendance():
         return self.isHere
