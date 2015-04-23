686463f1531e25fcde7496071dd3b215755f45e9
Bradford Smith <bsmith8@stevens.edu>
2015-04-22 21:22:43 -0400

    Added ac-attendance.py and emailer.py
    emailer.py is for notes don't submit this!!

---
+++ src/ac-attendance.py
@@ -0,0 +1,23 @@
+# Bradford Smith and David Ott
+# CS 347 Assignment 3
+# AbsenceCheck ac-attendance.py
+# "I pledge my honor that I have abided by the Stevens Honor System."
+#####################################################################
+
+#ac_attendance
+#this class is used to create and store a listOfStudents for taking attendance
+class ac_attendance:
+    listOfStudents = []
+
+    #pre:takes in a file f
+    #post:creates the listOfStudent variable from the students in the file
+    def parse_file(f):
+        #open file f
+        #create student object for each student name and email found
+        listOfStudents.append(new_student)
+
+    #pre:none
+    #post:returns the listOfStudents variable
+    def get_listOfStudents():
+        return self.listOfStudents
+
