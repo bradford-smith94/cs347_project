0aa6eee613c8772611af1b3a8f7ffc8539907875
Bradford Smith <bsmith8@stevens.edu>
2015-04-22 22:09:54 -0400

    Updated ac-attendance.py
    There is now a get_listOfAbsentStudents method

--- src/ac-attendance.py
+++ src/ac-attendance.py
@@ -8,16 +8,30 @@
 #this class is used to create and store a listOfStudents for taking attendance
 class ac_attendance:
     listOfStudents = []
+    listOfAbsentStudents = []
 
     #pre:takes in a file f
     #post:creates the listOfStudent variable from the students in the file
     def parse_file(f):
         #open file f
-        #create student object for each student name and email found
+        #create new_student object for each student name and email found
         listOfStudents.append(new_student)
 
     #pre:none
     #post:returns the listOfStudents variable
     def get_listOfStudents():
         return self.listOfStudents
+
+    #pre:none
+    #post:listOfAbsentStudents variable is filled with all students who are absent
+    def update_listOfAbsentStudents():
+        for i in self.listOfStudents:
+            if not i.get_attendance():
+                self.listOfAbsentStudents.append(i)
+
+    #pre:none
+    #post:returns listOfAbsentStudents variable
+    def get_listOfAbsentStudents():
+        update_listOfAbsentStudents
+        return self.listOfAbsentStudents
 
