1b883cc5f77ba19e25b26561164efc3d5f43318a
David <davidott94@gmail.com>
2015-04-22 21:33:48 -0400

    here is notify

---
+++ src/ac-notify.py
@@ -0,0 +1,23 @@
+# Bradford Smith and David Ott
+# CS 347 Assignment 3
+# AbsenceCheck ac-notify.py
+# "I pledge my honor that I have abided by the Stevens Honor System."
+#####################################################################
+
+#ac_notify
+#this class is used to send the config file to absent students
+
+class ac_notify:
+	#vars
+	attendance = ac_attendance();
+	admin = 
+	
+	def sender()
+	
+	def get_attendance()
+		attendance.listOfStudents;
+	
+	def get_admin()
+	
+	def get_email_body()
+	
