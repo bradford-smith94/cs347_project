6939b315fb888763e868890be309634eaa9ecd8a
Bradford Smith <bsmith8@stevens.edu>
2015-04-22 20:11:16 -0400

    Added the src directory and main.py
    main.py is empty for now, will be the program main method

---
+++ src/main.py
@@ -0,0 +1,8 @@
+# Bradford Smith and David Ott
+# CS 347 Assignment 3
+# AbsenceCheck main.py
+# "I pledge my honor that I have abided by the Stevens Honor System."
+#####################################################################
+
+def main():
+    return 0
