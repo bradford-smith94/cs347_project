de4236b5536129a2edc32536291459fc0f1cb64a
Bradford Smith <bsmith8@stevens.edu>
2015-04-24 11:39:41 -0400

    Started to update ac-notify
    Need to restructure how ac-notify works:
    	-get rid of get_attendance
    	-get rid of get_admin
    	-get rid of get_email_body
    	-do those ^ in the main
    	-have this class just form and send emails (see emailer.py)

--- src/ac-notify.py
+++ src/ac-notify.py
@@ -3,6 +3,7 @@
 # AbsenceCheck ac-notify.py
 # "I pledge my honor that I have abided by the Stevens Honor System."
 #####################################################################
+from ac_config import ac_config
 from ac_attendance import ac_attendance
 from student import student
 from person import person
@@ -11,19 +12,21 @@ from person import person
 #this class is used to send the config file to absent students
 class ac_notify:
 
-	#vars
-	attendance = ac_attendance();
-	admin = person()
-	student_name = student()
-	recipient = person()
+    #vars
+    attendance = ac_attendance();
+    admin = person()
+    student_name = student()
+    recipient = person()
+    config = ac_config()
 
-	def sender(self, recipient, student_name):
+    def sender(self, recipient, student_name):
+        print("TODO")
 
-	def get_attendance(self):
-		attendance.get_listOfStudents
+    def get_attendance(self):
+        attendance.get_listOfStudents
 
-	def get_admin(self):
-		return person
+    def get_admin(self):
+        return self.admin
 
-	def get_email_body(self):
-
+    def get_email_body(self):
+        return self.config.get_class_descrip()
