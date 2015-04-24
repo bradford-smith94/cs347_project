855b42cde441e1f6b80f29ef932e7799af258627
David <davidott94@gmail.com>
2015-04-24 15:17:20 -0400

    new main stuff

--- src/main.py
+++ src/main.py
@@ -15,10 +15,20 @@ def main():
     
     
     #promptig teacher to enter name
-    name = input("please enter your name: ")
-    teacher_name = get_name(name)
-    email = input("please enter your email")	
-	
+    name = raw_input("please enter your name: ")
+    type(name)
+    #teacher_name = get_name(name)
+    
+    #prompting teacher to enter email
+    email = raw_input("please enter your email: ")
+    type(email)
+    #teacher_email = get_email(email)	
+    
+    #prompting teacher to enter a class description
+    classDescript = raw_input("Please enter todays topics covered, assignments and related material: ")
+    type(classDescript)
+    
+			
 
 
 
