5018814fcbf816e143c57ee2d25f5bc7c54f6603
Bradford Smith <bsmith8@stevens.edu>
2015-04-24 11:30:32 -0400

    Updated test_people.py
    Test now checks admin class
    Also updated the README.txt to explain running tests

--- README.txt
+++ README.txt
@@ -2,14 +2,25 @@ CS 347: Software Development Processes
 Group: Bradford Smith and David Ott
 
 AbsenceCheck
+############
+
+Project Structure
 src/
 	-contains all the source code
 	-contains unit tests for Assignment 3: first iteration
 		-run test_*.py files and look for desired output
+            -e.g. $`python2 src/test_people.py`
+            (where $ is your prompt and the command is surrounded in `)
+            (note: python2 command is used because python2 and python3 are
+                installed, your command to run python may differ)
 doc/
-	-contains documentation
+	-contains documentation files
 	-!out of date!
 tests/
 	-contains acceptance testing code
 	-cucumber features
-	-!out of date!
+        -!out of date!
+            -does not yet have ruby step definitions
+emailer.py
+    -!!do not submit this!!
+    -this file is for our notes
--- src/test_people.py
+++ src/test_people.py
@@ -3,24 +3,60 @@
 # AbsenceCheck test_people.py
 # "I pledge my honor that I have abided by the Stevens Honor System."
 #####################################################################
+from person import person
 from student import student
+from admin import admin
 
+#class to test the person class and its descendents
+#   check to make sure that descendent classes implement inheritance correctly
 def main():
     student1 = student("John Smith", "jsmith@not-a-domain.tld")
     student2 = student("Name Surname Suffix", "name@host.com", False)
+    admin1 = admin("Joe Admin", "it@ti.com")
+    admin2 = admin("root", "root@localhost", False)
+    placeholder = person("", "")
 
-    print("Testing get name from person")
+    print("Testing get name from student")
     print(student1.get_name())
     print(student2.get_name())
+    print("Testing get name from admin")
+    print(admin1.get_name())
+    print(admin2.get_name())
+    print("Testing get name from person")
+    placeholder = student1
+    print(placeholder.get_name())
+    placeholder = student2
+    print(placeholder.get_name())
+    placeholder = admin1
+    print(placeholder.get_name())
+    placeholder = admin2
+    print(placeholder.get_name())
 
     print("")
-    print("Testing get email from person")
+    print("Testing get email from student")
     print(student1.get_email())
     print(student2.get_email())
+    print("Testing get email from admin")
+    print(admin1.get_email())
+    print(admin2.get_email())
+    print("Testing get email from person")
+    placeholder = student1
+    print(placeholder.get_email())
+    placeholder = student2
+    print(placeholder.get_email())
+    placeholder = admin1
+    print(placeholder.get_email())
+    placeholder = admin2
+    print(placeholder.get_email())
 
     print("")
     print("Testing get attendance from student")
     print(student1.get_attendance())
     print(student2.get_attendance())
+
+    print("")
+    print("Testing get request from admin")
+    print(admin1.get_admin_request())
+    print(admin2.get_admin_request())
 
 main()
