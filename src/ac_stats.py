a790a59253e12305711e0248b3088f2d2d492421
Bradford Smith <bsmith8@stevens.edu>
2015-05-02 13:29:33 -0400

    Started work on ac_stats
    Created the ac_stats.py file, started added method signatures.
    Need to update ac_notify to have a separate send function for sending to administrators.

---
+++ src/ac_stats.py
@@ -0,0 +1,33 @@
+# Bradford Smith and David Ott
+# CS 347 Assignment 3
+# AbsenceCheck ac_stats.py
+# "I pledge my honor that I have abided by the Stevens Honor System."
+#####################################################################
+#import csv to read and save .csv files
+import csv
+#import student and admin classes
+from student import student
+from admin import admin
+from ac_notify import ac_notify
+
+#ac_stats
+#this class is used to save cumulative attendance statistics to a csv file
+#   optionally this class will also notify an administrator if requested
+class ac_stats:
+    admin1
+
+    #pre: takes in an admin a
+    #post: sets the admin variable to a
+    def set_admin(self, a):
+        self.admin1 = a
+
+    #pre:none
+    #post:returns the admin variable
+    def get_admin(self):
+        return self.admin1
+
+    #pre:takes in a list of students lst, and optionally a filename
+    #post:saves cumulative absences in a csv file, and if admin.get_request
+    #   notifies the admin
+    def save_stats(self, lst, file="cumulative.csv"):
+        print("TODO")
